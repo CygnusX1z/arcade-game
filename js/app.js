71df1664b73a1c172c0f395f53425e83c68af4a9
Jorge Zuniga <jlzuniga001@att.net>
2015-01-10 17:10:59 -0600

    Add and edit comments to stay within 80 column limit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here, we've provided one for you to get started
-    // The image/sprite for our enemies, this uses a helper we've provided to easily load images
+    // Variables applied to each of our instances go here, 
+	// we've provided one for you to get started
+	
+    // The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.enemyY = [60,145,230];
     this.x = this.startPosX();
@@ -21,9 +24,12 @@ Enemy.prototype.startPosY = function() {
 	return startY;
 };
 
-// Update the enemy's position, required method for game Parameter: dt, a time delta between ticks
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers.	
+    // You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.	
 	this.x += this.speed[Math.round(Math.random()*5)]*dt;
 	if (this.x > 550) {
 		this.x = this.startPosX();
@@ -36,43 +42,48 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class. This class requires an update(), render() and a handleInput() method.
+// Now write your own player class. 
+// This class requires an update(), render() and
+// a handleInput() method.
 var Player = function() {
 	this.sprite = 'images/char-boy.png';
 	this.x = 200;
 	this.y = 430;
 };
 
+// Update the Player's position
 Player.prototype.update = function(dt) {
 	this.x*dt;
 	this.y*dt;
 };
 
+// Reset the Player's position
 Player.prototype.reset = function(){
 	this.x=200;
 	this.y=430;
 };
 
+// Draw the enemy on the screen, required method for game
 Player.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Players movements via up, down, left, right keyboard keys
 Player.prototype.handleInput = function(direction) {
 	if (direction === 'left' && this.x > 0) {
 		this.x -= 101;
-	}
-	else if (direction === 'right' && this.x < 400) {
+	} else if (direction === 'right' && this.x < 400) {
 		this.x += 101;
-	}
-	else if (direction === 'up' && this.y > 15) {
+	} else if (direction === 'up' && this.y > 15) {
 		this.y -= 83;
-	}
-	else if (direction === 'down' && this.y < 400) {
+	} else if (direction === 'down' && this.y < 400) {
 		this.y += 83;
 	}	
 };
 
-// Now instantiate your objects. Place all enemy objects in an array called allEnemies. Place the player object in a variable called player.
+// Now instantiate your objects. 
+// Place all enemy objects in an array called allEnemies. 
+// Place the player object in a variable called player.
 var enemy1 = new Enemy();
 var enemy2 = new Enemy();
 var enemy3 = new Enemy();
@@ -93,7 +104,7 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-
+// Reset the positions of Player & Enemies
 function resetPositions() {
 	player.reset();
 	for(var j in allEnemies) {
@@ -102,6 +113,7 @@ function resetPositions() {
 	}
 }
 
+// Check for collisions of Player and Enemies
 var checkCollisions = function(allEnemies,player) {
 	for (i in allEnemies) {
 	if (((allEnemies[i].x - player.x) < 80) 
